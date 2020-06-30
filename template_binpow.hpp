754da23b4e9c957bfacd996a6dc8e948026f1de8
SerebryakovMA <serebryakov17m@mail.ru>
2020-06-30 12:20:59 +0300

    competition by squaring on templates

--- template_binpow.hpp
+++ template_binpow.hpp
@@ -10,13 +10,13 @@ template<> Matrix identity<> (const Matrix& a) {
   return Matrix::identity(a.rows());
 }
 
-template <typename T> T binpow(const T& a, unsigned int n) {
-    if (n == 0)
+template <typename T, unsigned int n> T binpow(const T& a) {
+    if constexpr (n == 0)
       return identity(a);
-    if (n % 2 == 1)
-        return binpow(a, n-1)*a;
+    if constexpr (n % 2 == 1)
+        return binpow<n-1>(a) * a;
     else {
-        T b = binpow(a, n/2);
+        T b = binpow<n/2>(a);
         return b*b;
     }
 }
